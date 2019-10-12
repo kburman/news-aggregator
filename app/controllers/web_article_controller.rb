2b9d811c2ea14dd2278876de68934890f3bc2b23
kshitij.burman <kshitij.burman@tech9.com>
2019-10-13 04:31:02 +0530

    Added search field and fixed large will_paginate in mobile devices.

--- app/controllers/web_article_controller.rb
+++ app/controllers/web_article_controller.rb
@@ -1,9 +1,19 @@
 class WebArticleController < ApplicationController
+  before_action :find_articles, only: [:index]
+
   def index
-    @web_articles = WebArticle.order(created_at: :desc).includes(web_link: :web_domain).paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @web_article = WebArticle.find(params[:id])
+  end
+
+  private
+
+  def find_articles
+    @search_term = params.dig(:web_article, :title)
+    @web_articles = WebArticle.order(created_at: :desc).includes(web_link: :web_domain)
+    @web_articles = @web_articles.where("title ILIKE ?", "% %#{@search_term}%") if @search_term.present?
+    @web_articles = @web_articles.paginate(page: params[:page])
   end
 end
