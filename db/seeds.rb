7e85930e08d09fbab37ae72122601296d13ff954
kshitij.burman <kshitij.burman@tech9.com>
2019-10-12 18:00:40 +0530

    Added `mensxp.com` web client.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 SCRAPER_KLASS = [
-  WebClients::TimesOfIndiaClient
+  WebClients::TimesOfIndiaClient,
+  WebClients::MenXpClient
 ]
 
 SCRAPER_KLASS.each do |klass|
   base_uri = URI.parse(klass.base_uri)
-  web_domain = WebDomain.create!(domain_name: base_uri.host)
-  ScrapeService.create!(web_domain: web_domain, scraper_klass_fq_name: klass.name)
-  WebLink.create!(web_domain: web_domain, path: '/', scheme: 'https', port: base_uri.port)
+  web_domain = WebDomain.find_or_create_by!(domain_name: base_uri.host)
+  ScrapeService.find_or_create_by!(web_domain: web_domain, scraper_klass_fq_name: klass.name)
+  WebLink.find_or_create_by!(web_domain: web_domain, path: '/', scheme: 'https', port: base_uri.port)
 end
