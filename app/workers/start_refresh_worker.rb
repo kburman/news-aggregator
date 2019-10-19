d81d54e2b8b17172e8f8f610063dd3ed15f8c382
kshitij.burman <kshitij.burman@tech9.com>
2019-10-20 00:37:38 +0530

    Removed old workers.

--- app/workers/start_refresh_worker.rb
+++
@@ -1,7 +0,0 @@
-class StartRefreshWorker
-  include Sidekiq::Worker
-
-  def perform
-    ScrapeService.all.pluck(:id).each { |scrape_service_id| StartScrapeServiceAsync.call(scrape_service_id) }
-  end
-end
