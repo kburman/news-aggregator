9914a6fd5f809f7b13db51bf2bb6b69ca86f13dd
kshitij.burman <kshitij.burman@tech9.com>
2019-10-20 00:45:36 +0530

    Added test file.

--- test/models/seed_url_test.rb
+++ test/models/seed_url_test.rb
@@ -5,3 +5,19 @@ class SeedUrlTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+# Schema version: 20191019162534
+#
+# Table name: seed_urls
+#
+#  id              :integer          not null, primary key
+#  web_link_id     :integer          not null
+#  last_scraped_at :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_seed_urls_on_web_link_id  (web_link_id) UNIQUE
+#
--- test/models/web_content_test.rb
+++ test/models/web_content_test.rb
@@ -5,3 +5,23 @@ class WebContentTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+# Schema version: 20191019162534
+#
+# Table name: web_contents
+#
+#  id               :integer          not null, primary key
+#  web_link_id      :integer          not null
+#  response_headers :text
+#  response_body    :binary
+#  body_size        :integer
+#  content_type     :text
+#  scraped_at       :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_web_contents_on_web_link_id  (web_link_id)
+#
